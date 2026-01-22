3fd1edff2b59a6b3f60fc33e562dd284adf6c989
Giang <12815698+rivful@users.noreply.github.com>
2026-01-22 20:44:40 +1100

    add script for crawler and update duckdb

--- db/db.py
+++ db/db.py
@@ -4,3 +4,32 @@ path = "jobs.duckdb"
 
 def get_connection():
     return duckdb.connect(path)
+
+def insert_job(job):
+    conn = get_connection()
+    conn.execute(
+        """
+        INSERT INTO jobs (
+            job_id,
+            job_url,
+            title,
+            company,
+            loc,
+            salary,
+            description,
+            ts
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            job["job_id"],
+            job["job_url"],
+            job["title"],
+            job["company"],
+            job["location"],
+            job["salary"],
+            job["description"],
+            job["ts"],
+        )
+    )
+    conn.close()
