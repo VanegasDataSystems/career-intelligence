1c39864bfe96db31c44d702b6dd9c01f22988673
P360 <vanegasdatasystems@gmail.com>
2026-01-23 22:06:05 -0500

    selects.sql

--- fv/sql/selects.sql
+++ fv/sql/selects.sql
@@ -1,2 +1,7 @@
-select * from job_listings.remoteok_jobs;
-select * from job_listings.remoteok_jobs__tags;
+select j.id as job_id, j.salary_min, j.salary_max, j.location, j.position, j.company, j.date, j.url, count(t.value) as tagCount,
+       STRING_AGG(t.value, ', ') AS tags_string
+from job_listings.remoteok_jobs j
+
+left outer join job_listings.remoteok_jobs__tags t
+on j."_dlt_id" = t."_dlt_parent_id"
+GROUP BY ALL
